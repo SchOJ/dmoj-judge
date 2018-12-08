85edeef83313888bef516e6cd16c67cd918b5532
Outvi V <olvi.git@mailnull.com>
2018-12-08 19:14:51 +0800

    Fix permission of dmoj

--- startup.sh
+++ startup.sh
@@ -4,6 +4,8 @@ if [ ! -e /install_done ]; then
 problem_storage_root:
 - /problems
 ' >> /config.yml
+  mkdir -p /home/judge/.cache/Python-Eggs
+  chown judge:judge -R /home/judge
   touch /install_done
 fi
 su judge -c 'dmoj -c /config.yml site $JUDGE_NAME $JUDGE_KEY'
